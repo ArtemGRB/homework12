0ce73d15c193e8e0474c7306f7dcf2fb0faa383f
ArtemGRB <artemgrb@gmail.com>
2025-02-25 22:14:38 +0700

    hw12

--- src/App.java
+++ src/App.java
@@ -1,9 +1,11 @@
-public class Main {
+public class App {
     public static void main(String[] args) {
         Author author1 = new Author("Александр", "Волков");
         Author author2 = new Author("Маленький принц", "де Сент-Экзюпери");
         Book book1 = new Book("Маленький принц", author2, 1943);
         Book book2 = new Book("Волшебник Изумрудного города", author1, 1939);
 
+        book1.setYearOfPublication(1938);
+        System.out.println(book1.getYearOfPublication());
     }
 }
---
+++ src/Book.java
@@ -0,0 +1,27 @@
+public class Book {
+    String titleBook;
+    Author author;
+    int yearOfPublication;
+
+    public Book(String titleBook, Author author, int yearOfPublication) {
+        this.titleBook = titleBook;
+        this.author = author;
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    public String getTitleBook() {
+        return titleBook;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getYearOfPublication() {
+        return yearOfPublication;
+    }
+
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+}
